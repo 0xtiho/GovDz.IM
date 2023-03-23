906d293bb7a0bf296fc5324f982cedacc62d8cb8
GovDz <95767427+GovDz@users.noreply.github.com>
2023-03-23 15:28:14 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>- GovDz - </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:type" content="website">
+	<!-- All the weird CSS // JS tags are just to mess with bots trying to scrape emails. -->
+	<style>.email,.xmpp{content: "_not_a_real_address_@xmpp.jp";display: none;}</style>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/open-fonts@1.1.1/fonts/inter.css" integrity="sha256-QOqJnVXqSKzjk5O4ngMED3Pd2o3S4Nfkieede4b6+Pc=" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.3/new.css" integrity="sha256-PyPTJCAMwPQISGrpBIffj9UHOZKnACvcLiyc3lYANdc=" crossorigin="anonymous" referrerpolicy="no-referrer">
+</head>
+<body>
+    <header>
+        <h1>GovDz<img src="/images/logo.png" style="margin-bottom:-2px;width: 41px;"></h1>
+        <nav>
+            <a href="https://govdz.im" rel="noreferrer" target="_blank">Home</a> /
+            <a href="http://www.zone-h.org/archive/notifier=tiho-dz" rel="noreferrer" target="_blank">Zone-H</a> /
+            <a href="https://t.me/Gov_Dz" rel="noreferrer" target="_blank">Telegram</a> / 
+            <a href="https://zelenka.guru/gangaster/" rel="noreferrer" target="_blank">LolzGuru</a>
+        </nav>
+    </header>
+	<blockquote>
+        <i>This site is made from vanilla HTML and the default <a href="https://newcss.net/" rel="noreferrer" target="_blank">new.css</a> theme.</i>
+    </blockquote>
+    <h1></h1>
+    <p>Website Administrator.</p>
+    <p>Feel free to use PGP When sending me messages over any platforms. My key is listed below, and is also at <a href="https://govdz.im/pgp.txt">https://govdz.im/pgp.txt</a>.</p>
+	<details>
+		<summary>Public PGP Key<code style="margin-left:10px;">7B40 822D BBFD 0EE1 8B7A 3CEC AAE4 E46F DF71 EC04</code></summary>
+			<pre>-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+mDMEZByA/xYJKwYBBAHaRw8BAQdAgs4OayKduq0qPLhK8hlX24iUItCC/7WuFDbb
+84yDBtC0F2dvdmR6IDxnMHZkekBwcm90b24ubWU+iJYEExYIAD4WIQR7QIItu/0O
+4Yt6POyq5ORv33HsBAUCZByA/wIbAwUJA8JnAAULCQgHAgYVCgkICwIEFgIDAQIe
+AQIXgAAKCRCq5ORv33HsBKjFAQCRmrwBDJL+7QVU+b8srtR/08mrBrbkZhpA9Rk8
+kq/rWAEAu1JLCb/ftMkqxYxkRmlA9Nrhonsp7MoyWm83Ls9vVQy4OARkHID/Egor
+BgEEAZdVAQUBAQdAEkT63iDIdcl1IqxrA1dgx9iP1C00M7XCY9IeBJJFBkcDAQgH
+iH4EGBYIACYWIQR7QIItu/0O4Yt6POyq5ORv33HsBAUCZByA/wIbDAUJA8JnAAAK
+CRCq5ORv33HsBN0+AQC5tINfUIDjIh81MKSO9yj449ao9+iQIrrZ8PG50+v31gEA
+mCRY9/dqqjSb4ckUvs+pInYrdJvxq1GJQJVPEUU6fAI=
+=bHkd
+-----END PGP PUBLIC KEY BLOCK-----
+</pre>
+	</details>
+    <br>
+    <h2>Contact Methods</h2>
+    <table>
+        <tr>
+            <td style="border: 1px solid #554f4f"><strong>Username</strong></td>
+            <td style="border: 1px solid #554f4f"><strong>Platform</strong></td>
+        </tr>
+        <tr>
+            <td><a href="https://twitter.com/GovDzFU" rel="noreferrer" target="_blank">@GovDzFU</a></td>
+            <td>Twitter</td>
+        </tr>
+        <tr>
+            <td><a href="https://t.me/Gov_Dz" rel="noreferrer" target="_blank">@Gov_Dz</a></td>
+            <td>Telegram</td>
+        </tr>
+        <tr>
+            <td><a rel="me" href="https://zelenka.guru/gangaster/">@Gangaster</a></td>
+            <td>Mastodon</td>
+        <tr>
+            <td>g0vdz<span>&#x40;<script>//</span><span>eee@invalid.net</span></script><span class="email">contactme_123</span>pro<span class="xmpp">govzzd@gmail.com</span>to<span>n&period;<span style="display: none;"><a class="email">email(.)</a>.jp<span></span>com</span>&#109;&#101;</span></span></td>
+            <td>Email (PGP Encrypt if needed)</td>
+        </tr>
+    </table>
+
+	<p>Email is the best way to contact me for quick questions, Telegram for anything more complicated.</p>
+	<h2>Privacy</h2>
+    <p>I collect no personally identifiable information about you. Cloudflare isn't used on this part of my website either, however it is used on my blog (To block attacks). You can read the privacy Policy of Cloudflare <a href="https://www.cloudflare.com/privacypolicy/" rel="noreferrer" target="_blank">here</a> if you wish.</p><br>
+    <hr>
+</body>
+</html>
